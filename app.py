e9874b72eb269f3da0f7bff50025875e040a44f0
Smugcurve13 <sambhavsoni14@gmail.com>
2024-11-14 10:49:01 +0530

    added store endpoints

--- app.py
+++ app.py
@@ -33,4 +33,18 @@ def create_item(name):
             new_item = {"name": request_data["name"], "price": request_data["price"]}
             store['items'].append(new_item)
             return new_item,201
-    return {"message":"Store Not Found"}
+    return {"message":"Store Not Found"}, 404
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store['name']== name:
+            return store
+    return {"message":"Store Not Found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store['name']== name:
+            return {"items": store["items"]}
+    return {"message":"Store Not Found"}, 404
